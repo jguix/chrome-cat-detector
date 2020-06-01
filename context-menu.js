6fa2fff6b5d32057fbf35f548c2fcc4933758b12
Juan G. Jordán <juangui@gmail.com>
2020-06-02 00:43:19 +0200

    Refactoring

--- context-menu.js
+++ context-menu.js
@@ -1,22 +1,25 @@
-chrome.storage.sync.get(["autodetect"], ({ autodetect }) => {
-  if (autodetect === undefined) {
-    chrome.storage.sync.set({ autodetect: false });
-  }
-});
+const setDefaults = () =>
+  chrome.storage.sync.get(["autodetect"], ({ autodetect }) => {
+    if (autodetect === undefined) {
+      chrome.storage.sync.set({ autodetect: false });
+    }
+  });
 
-chrome.contextMenus.removeAll();
-chrome.contextMenus.create({
-  id: "DETECT_CATS",
-  title: "Detect cats",
-  contexts: ["browser_action"],
-});
-chrome.contextMenus.create({
-  id: "AUTO_DETECT",
-  title: "Auto-detect",
-  type: "checkbox",
-  checked: false,
-  contexts: ["browser_action"],
-});
+const setMenuItems = () => {
+  chrome.contextMenus.removeAll();
+  chrome.contextMenus.create({
+    id: "DETECT_CATS",
+    title: "Detect cats",
+    contexts: ["browser_action"],
+  });
+  chrome.contextMenus.create({
+    id: "AUTO_DETECT",
+    title: "Auto-detect",
+    type: "checkbox",
+    checked: false,
+    contexts: ["browser_action"],
+  });
+};
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "DETECT_CATS") {
@@ -25,3 +28,6 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.storage.sync.set({ autodetect: info.checked });
   }
 });
+
+setDefaults();
+setMenuItems();
