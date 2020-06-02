f8b2df12b0b97fdab6a8be71856e1403e7171129
Juan G. Jordán <juangui@gmail.com>
2020-06-03 01:33:18 +0200

    Enhanced regex and URL match pattern

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+const words = ["cat", "cats", "kitten", "kittens", "kitty", "kitties"];
+const regex = new RegExp(`(${words.join("|")})\\b`, "gi");
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.text === "cat_count") {
     sendResponse(countCats());
@@ -8,7 +11,6 @@ const countCats = () => {
   var content =
     document.body["innerText" in document.body ? "innerText" : "textContent"];
   content = removeScriptsFromContent(content);
-  var regex = /(cat|cats|kitten|kittens|kitty|kitties)[\s.,]/gi;
 
   return content.match(regex)?.length || 0;
 };
